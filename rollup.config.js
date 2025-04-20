4b2b8dfdf47a1d19cad5d1278632b07cd294bf63
terence <terence.g@binance.com>
2025-04-20 19:36:45 +0800

    add replacement plugin for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,10 @@ import terser from '@rollup/plugin-terser';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
 import alias from '@rollup/plugin-alias'
+import replace from '@rollup/plugin-replace'
 
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -33,6 +33,12 @@ export default {
       entries: [
         { find: '@', replacement: path.resolve(__dirname, 'src') },
       ]
+    }),
+
+    replace({
+      preventAssignment: true, // 必须加！否则变量替换可能会出 bug
+      'process.env.NODE_ENV': JSON.stringify(isProduction ? 'prod' : 'env'),
+      __APP_VERSION__: JSON.stringify('1.0.0') // 你也可以自定义变量
     }),
     
     // 只在生产环境压缩代码
